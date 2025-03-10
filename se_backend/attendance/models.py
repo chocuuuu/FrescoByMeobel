732747ee6300347e0aa69f7ce4c61c6770eddea1
Nicoz <monsteronicz@gmail.com>
2025-03-10 13:23:13 +0800

    se-backend added attendance -> attendance summary automation

--- se_backend/attendance/models.py
+++ se_backend/attendance/models.py
@@ -2,14 +2,15 @@ from django.db import models
 
 from biometricdata.models import BiometricData
 
+from users.models import CustomUser
 
 class Attendance(models.Model):
     id = models.AutoField(primary_key=True)
-    biometric_data_id = models.ForeignKey(BiometricData, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True)
     date = models.DateField()
     status = models.CharField(max_length=255)
     check_in_time = models.TimeField()
     check_out_time = models.TimeField()
 
     def __str__(self):
-        return f"{self.id} - {self.biometric_data_id}"
+        return f"{self.id} - {self.user_id}"
