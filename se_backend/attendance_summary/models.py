f31d51953c49a93f9d84f766e73ced5d553b67fe
Nicoz <monsteronicz@gmail.com>
2025-03-10 13:23:38 +0800

    se-backend redefined attendance summary

--- se_backend/attendance_summary/models.py
+++ se_backend/attendance_summary/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 from attendance.models import Attendance
+from users.models import CustomUser
 
 
 class AttendanceSummary(models.Model):
     id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     attendance_id = models.ForeignKey(Attendance, on_delete=models.CASCADE)
     date = models.DateField()
     actual_hours = models.IntegerField()
--- se_backend/attendance_summary/serializers.py
+++ se_backend/attendance_summary/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import AttendanceSummary
 
 class AttendanceSummarySerializer(serializers.ModelSerializer):
+
     class Meta:
         model = AttendanceSummary
         fields = '__all__'
