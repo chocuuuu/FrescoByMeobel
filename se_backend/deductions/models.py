1fba43f6997978efb598cec3b4cdde85af23b39e
Nicolai <69515475+NicozAlcaraz@users.noreply.github.com>
2025-03-30 18:51:38 +0800

    se-backend removed benefits from deductions

--- se_backend/deductions/models.py
+++ se_backend/deductions/models.py
@@ -6,10 +6,6 @@ from users.models import CustomUser
 class Deductions(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True)
-    # attendancesummary_id = models.ForeignKey(AttendanceSummary, on_delete=models.CASCADE, null=True)
-    sss = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-    philhealth = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-    pagibig = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     wtax = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     nowork = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     late = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
