02e42be100d5aee44ab10c32e2f072bb07d224f4
Nicolai <69515475+NicozAlcaraz@users.noreply.github.com>
2025-04-06 20:54:53 +0800

    se-backend updated benefits computation to be divided by 2

--- se_backend/shared/computations/philhealth_computations.py
+++ se_backend/shared/computations/philhealth_computations.py
@@ -3,7 +3,7 @@ from decimal import Decimal
 def compute_philhealth_contribution(basic_salary):
 
     basic_salary = Decimal(basic_salary)
-    total_contribution = basic_salary * Decimal(.05)
+    total_contribution = basic_salary * Decimal(.05) / 2
 
     return {
         "Basic Salary": Decimal(basic_salary),
